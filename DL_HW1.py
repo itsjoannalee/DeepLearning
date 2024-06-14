80762c37eb2455e58791bec64493f99fc6aac8b5
itsjoannalee <109008199+itsjoannalee@users.noreply.github.com>
2024-06-14 22:07:46 +0800

    Add files via upload

---
+++ DL_HW1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import time
+
+data1_raw = pd.read_csv("D:\研究所課程\深度\DL_HW1\energy_efficiency_data.csv")
+data1_raw
+
+data1 = pd.get_dummies(data1_raw, columns=['Orientation', 'Glazing Area Distribution'])
+data1
